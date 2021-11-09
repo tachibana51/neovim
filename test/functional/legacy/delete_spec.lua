be8abb2704a91bb661ad5a66051618d094d8c155
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-11-09 09:56:51 +0900

    vim-patch:8.1.1378: delete() can not handle a file name that looks like a pattern
    Problem:    Delete() can not handle a file name that looks like a pattern.
    Solution:   Use readdir() instead of appending "/*" and expanding wildcards.
                (Ken Takata, closes vim/vim#4424, closes vim/vim#696)
    https://github.com/vim/vim/commit/701ff0a3e53d253d7300c385e582659bbff7860d

--- test/functional/legacy/delete_spec.lua
+++ test/functional/legacy/delete_spec.lua
@@ -25,25 +25,6 @@ describe('Test for delete()', function()
     eq(0, eval("isdirectory('Xdir1')"))
     eq(-1, eval("delete('Xdir1', 'd')"))
   end)
-  it('recursive delete', function()
-    command("call mkdir('Xdir1')")
-    command("call mkdir('Xdir1/subdir')")
-    command("call mkdir('Xdir1/empty')")
-    command('split Xdir1/Xfile')
-    command("call setline(1, ['a', 'b'])")
-    command('w')
-    command('w Xdir1/subdir/Xfile')
-    command('close')
-
-    eq(1, eval("isdirectory('Xdir1')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir1/Xfile')"))
-    eq(1, eval("isdirectory('Xdir1/subdir')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir1/subdir/Xfile')"))
-    eq(1, eval("'Xdir1/empty'->isdirectory()"))
-    eq(0, eval("delete('Xdir1', 'rf')"))
-    eq(0, eval("isdirectory('Xdir1')"))
-    eq(-1, eval("delete('Xdir1', 'd')"))
-  end)
 
   it('symlink delete', function()
     source([[
@@ -78,42 +59,6 @@ describe('Test for delete()', function()
     eq(0, eval("delete('Xlink')"))
     eq(-1, eval("delete('Xlink')"))
     eq(0, eval("delete('Xdir1', 'd')"))
-  end)
-
-  it('symlink recursive delete', function()
-    source([[
-      call mkdir('Xdir3')
-      call mkdir('Xdir3/subdir')
-      call mkdir('Xdir4')
-      split Xdir3/Xfile
-      call setline(1, ['a', 'b'])
-      w
-      w Xdir3/subdir/Xfile
-      w Xdir4/Xfile
-      close
-      if has('win32')
-        silent !mklink /j Xdir3\Xlink Xdir4
-      else
-        silent !ln -s ../Xdir4 Xdir3/Xlink
-      endif
-    ]])
-
-    eq(1, eval("isdirectory('Xdir3')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir3/Xfile')"))
-    eq(1, eval("isdirectory('Xdir3/subdir')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir3/subdir/Xfile')"))
-    eq(1, eval("isdirectory('Xdir4')"))
-    eq(1, eval("isdirectory('Xdir3/Xlink')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir4/Xfile')"))
-
-    eq(0, eval("delete('Xdir3', 'rf')"))
-    eq(0, eval("isdirectory('Xdir3')"))
-    eq(-1, eval("delete('Xdir3', 'd')"))
-    -- symlink is deleted, not the directory it points to
-    eq(1, eval("isdirectory('Xdir4')"))
-    eq(eval("['a', 'b']"), eval("readfile('Xdir4/Xfile')"))
-    eq(0, eval("delete('Xdir4/Xfile')"))
-    eq(0, eval("delete('Xdir4', 'd')"))
   end)
 
   it('gives correct emsgs', function()
